4bb9bfcf6f3d5425914551ecb852533c5fee6f9b
TonyDerek-dot <yu1429422734@gmail.com>
2025-11-28 19:29:57 +0800

    chore: yml file update

--- docker/buildx-and-push.sh
+++ docker/buildx-and-push.sh
@@ -25,11 +25,8 @@ docker buildx build \
     -f "$SCRIPT_DIR/Dockerfile" \
     --platform=$PLATFORM \
     --progress=plain \
-    -t "documenso/documenso:latest" \
-    -t "documenso/documenso:$GIT_SHA" \
-    -t "documenso/documenso:$APP_VERSION" \
-    -t "ghcr.io/documenso/documenso:latest" \
-    -t "ghcr.io/documenso/documenso:$GIT_SHA" \
-    -t "ghcr.io/documenso/documenso:$APP_VERSION" \
+    -t "fundconnecthk:latest" \
+    -t "fundconnecthk:$GIT_SHA" \
+    -t "fundconnecthk:$APP_VERSION" \
     --push \
     "$MONOREPO_ROOT"
